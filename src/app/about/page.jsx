e4cf9ecdc7ca71e6bbaa9f793675dfeeee1a057f
Hamed-Ajaj <hamedsgaming@gmail.com>
2024-07-30 16:05:30 +0300

    fixing about page...

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
     <section className='min-h-screen space-y-5 px-4 py-10 md:px-20'>
         <div className="flex flex-col lg:flex-row justify-between h-auto mb-10 p-10">
           <div className="w-full lg:w-[45%] flex flex-col justify-center gap-12 leading-7 md:p-8">
-            <h1 className="text-[54px] font-semibold">Our Story</h1>
+            <h1 className="text-[40px] text-nowrap sm:text-[48] md:text-[54px] font-semibold">Our Story</h1>
             <p>
               Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. 
             </p>
