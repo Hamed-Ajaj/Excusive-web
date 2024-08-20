631b31d8e73dda999b06ddc4b831394ac9058898
Hamed-Ajaj <hamedsgaming@gmail.com>
2024-08-20 18:58:08 +0300

    trying to fix the bug

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -3,7 +3,7 @@ import { categoryFilter, colors, sizes } from "@/constants";
 import { FilterIcon } from "lucide-react";
 import { X } from "lucide-react";
 import { useState } from "react";
-// import FilterMenu from "./filterMenu";
+import FilterMenu from "./filterMenu";
 const FilterComponent = () => {
   const [activeSize, setActiveSize] = useState(null);
   const [activeColor, setActiveColor] = useState(null);
@@ -77,7 +77,92 @@ const FilterComponent = () => {
         </div>
         <hr className="w-full" />
       </div>
-      
+      {/* category filter */}
+      <FilterMenu key={"category"} filter={"Category"}>
+        <div>
+          <ul className="py-2  space-y-1 accent-black">
+          {categoryFilter?.map((category,index) => (
+            <div key={index} className="flex gap-3 items-center">
+              <input
+                type="checkbox"
+                name={category.name}
+                id={category.name}
+                className="h-4 w-4"
+                onChange={handleCheckBox}
+                checked={category.checked}
+              />
+              <label htmlFor="category1">{category.name}</label>
+            </div>
+          ))}
+          </ul>
+        </div>
+      </FilterMenu>
+      {/* status filter */}
+      <FilterMenu filter={"Status"}>
+        <div>
+          <ul className="py-2  space-y-1 accent-black">
+            {/* create two radio button in stock and out of stock */}
+            <div className="flex gap-3 items-center">
+              <input
+                type="radio"
+                name="status"
+                id="inStock"
+                className="h-4 w-4"
+              />
+              <label htmlFor="inStock">In Stock</label>
+            </div>
+            <div className="flex gap-3 items-center ">
+              <input
+                type="radio"
+                name="status"
+                id="outOfStock"
+                className="h-4 w-4"
+              />
+              <label htmlFor="outOfStock">Out of Stock</label>
+            </div>
+          </ul>
+        </div>
+      </FilterMenu>
+      {/* size filter */}
+      <FilterMenu filter={"Size"}>
+        <div className="flex flex-wrap mt-5 gap-3 items-center">
+          {sizes.map((size) => (
+            <div
+              key={size.id}
+              onClick={() => handleChangeSize(size.size)}
+              className={`w-[35px] p-2 h-[35px] cursor-pointer ${
+                activeSize === size.size
+                  ? "bg-[#db4444] border-none text-white"
+                  : "text-black"
+              }  text-[14px] font-medium flex justify-center items-center border border-black rounded-[5px]`}
+            >
+              {size.size}
+            </div>
+          ))}
+        </div>
+      </FilterMenu>
+      {/* color filter */}
+      <FilterMenu filter={"Color"}>
+        <div className="flex gap-4 mt-5 flex-wrap items-center">
+          {colors.map(({ id, color }) => (
+            <div
+              key={id}
+              onClick={() => handleChangeColor(color)}
+              className={`w-[30px] h-[30px] flex justify-center items-center cursor-pointer bg-${color} bg-black rounded-full`}
+            >
+              <div className="w-full flex justify-center items-center">
+                <img
+                  src="/icons/selected.svg"
+                  alt="selected"
+                  className={` ${
+                    activeColor === color ? "block" : "hidden"
+                  } w-[60%] object-cover`}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </FilterMenu>
     </nav>
   );
 };
